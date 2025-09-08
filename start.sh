a53881e1fe411e8bb024c484ca292d5a00c02522
RTWfranklin <gaspardrtw@gmail.com>
2025-09-08 21:51:24 +0200

    start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-
-# --- Mettre à jour les paquets ---
 apt-get update
-
-# --- Installer Chromium et le driver correspondant ---
 apt-get install -y chromium chromium-driver
-
-# --- Installer les dépendances Python ---
 pip install --upgrade pip
 pip install -r requirements.txt
-
-# --- Lancer le bot Python ---
 python3 bot.py
