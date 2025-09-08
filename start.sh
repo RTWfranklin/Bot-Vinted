c6cae380e5825efff9322c97a1d4e68b7486bbff
RTWfranklin <gaspardrtw@gmail.com>
2025-09-08 21:53:34 +0200

    start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+
+# Mettre à jour les paquets
 apt-get update
-apt-get install -y chromium chromium-driver
+
+# Installer Chromium et toutes les dépendances nécessaires
+apt-get install -y chromium chromium-driver libnss3 libgconf-2-4 fonts-liberation libappindicator3-1 xdg-utils
+
+# Installer les dépendances Python
 pip install --upgrade pip
 pip install -r requirements.txt
+
+# Lancer le bot
 python3 bot.py
