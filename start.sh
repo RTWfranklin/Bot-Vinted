8d00d0996a8b51ea572e2703b8c7a17cb2b08e9a
RTWfranklin <gaspardrtw@gmail.com>
2025-09-09 10:04:26 +0200

    start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-# Installer Chromium si pas déjà présent
 apt-get update
 apt-get install -y chromium chromium-driver
 
-# Lancer le bot
-python bot.py
+echo "==== PATH Chromium ===="
+which chromium || which chromium-browser || which google-chrome
+echo "==== PATH Chromedriver ===="
+which chromedriver
+
+python3 bot.py
+
