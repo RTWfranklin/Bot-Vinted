3effc7445b6220f8fa7e16a9509fbad456faef50
RTWfranklin <gaspardrtw@gmail.com>
2025-09-10 12:15:05 +0200

    start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-python3 bot.py
+# --- Script de lancement du bot Discord ---
+
+echo "🔹 Lancement du bot..."
+
+# Optionnel : définir le chemin de Chromium explicitement
+export CHROMIUM_BIN=/usr/bin/chromium
+
+python bot.py
